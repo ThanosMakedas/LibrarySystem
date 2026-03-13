39deabcc905d4b91cfbce73485956892b50c1703
ThanosMakedas <makedasathanasios@gmail.com>
2026-03-13 10:47:31 +0100

    Lägg till testdata och visa böcker i Blazor

--- LibrarySystem.Web/Program.cs
+++ LibrarySystem.Web/Program.cs
@@ -1,3 +1,4 @@
+using LibrarySystem.Core.Models;
 using LibrarySystem.Data;
 using LibrarySystem.Web.Components;
 using Microsoft.EntityFrameworkCore;
@@ -27,5 +28,42 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<LibraryContext>();
+
+    if (!db.Books.Any())
+    {
+        db.Books.AddRange(
+            new Book
+            {
+                ISBN = "9780131103627",
+                Title = "The C Programming Language",
+                Author = "Kernighan & Ritchie",
+                PublishedYear = 1988,
+                IsAvailable = true
+            },
+            new Book
+            {
+                ISBN = "9780201633610",
+                Title = "Design Patterns",
+                Author = "Gamma et al.",
+                PublishedYear = 1994,
+                IsAvailable = true
+            },
+            new Book
+            {
+                ISBN = "9780132350884",
+                Title = "Clean Code",
+                Author = "Robert C. Martin",
+                PublishedYear = 2008,
+                IsAvailable = true
+            }
+        );
+
+        db.SaveChanges();
+    }
+}
 
 app.Run();
