a9fbd19ea29e10c639ccce1cce39fafce6f077da
ThanosMakedas <makedasathanasios@gmail.com>
2026-03-13 09:55:50 +0100

    text Lägg till LibraryContext och konfigurera EF Core med SQL Server

---
+++ LibrarySystem.Data/LibraryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LibrarySystem.Core.Models;
+
+namespace LibrarySystem.Data;
+
+public class LibraryContext : DbContext
+{
+    public DbSet<Book> Books => Set<Book>();
+    public DbSet<Member> Members => Set<Member>();
+    public DbSet<Loan> Loans => Set<Loan>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(
+            "Server=(localdb)\\mssqllocaldb;Database=LibrarySystemDB;Trusted_Connection=True;"
+        );
+    }
+}
